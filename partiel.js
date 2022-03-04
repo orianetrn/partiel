f6fb86f50280096070e66f0aeaff639a5e0ffe6c
Oriane Tourneroche <orianetourneroche@MacBook-Pro-2.local>
2022-03-04 16:53:03 +0100

    save1

--- partiel.js
+++ partiel.js
@@ -12,9 +12,12 @@ formulaire.onsubmit = submit;
         const date = document.getElementById('date');
     
         let tacheFaire = document.getElementById('tacheFaire');
-        
-        //var chose = tacheFaire.insertBefore(document.createElement("div" ),tacheFaire.firstChild);
-        //chosei.innerHTML += '<button onClick="supr'+i+'()"> <ul><li>' + nomTache.value + ' ' + categorie.value + ' ' + date.value + '</li></ul></button>'; 
+
+        // tentative création div dans div pour tjr avoir la première div et donc tjr pouvoir inserer des taches (pb : ça ne marche pas)
+        /*
+        var chose = tacheFaire.insertBefore(document.createElement("div" ),tacheFaire.firstChild);
+        chosei.innerHTML += '<button onClick="supr'+i+'()"> <ul><li>' + nomTache.value + ' ' + categorie.value + ' ' + date.value + '</li></ul></button>';
+        */ 
     
         tacheFaire.innerHTML += '<button onClick="supr()"> <ul><li>' + nomTache.value + ' ' + categorie.value + ' ' + date.value + '</li></ul></button>'; 
     
@@ -24,8 +27,17 @@ formulaire.onsubmit = submit;
 
 function supprimer() {
     tacheFaire.remove();
-} 
 
+    // tentative création d'une nouvelle div (echec)
+    tacheFaire = document.createElement("div");
+    tacheFaire.elt.id = idStr; 
+};
+
+// bouton pour supprimer une tache (pb : supprime toutes les taches puis plus possible d'en rajouter)
+
+function supr() {
+    tacheFaire.remove();
+};
 
 
 
